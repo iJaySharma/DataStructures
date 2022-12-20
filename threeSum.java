d9d8b165419f3562d8133cf6f5f5c1b4e86e1e4f
iJaySharma <jay.sharma96100@gmail.com>
2022-12-20 06:40:25 -0800

    first commit

---
+++ threeSum.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static List<List<Integer>> threeSum(int[] nums) {
+     
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i =0;i<nums.length-2;i++){
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left= i+1;
+            int right = nums.length-1;
+            while(left<right){
+                int currentSum = nums[i]+nums[left]+nums[right];
+                if(currentSum==0){
+                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    left+=1;
+                    right-=1;
+                while (left < right && nums[right] == nums[right + 1]) right--;
+                }
+                
+                
+                else if(currentSum<0){
+                    left+=1;
+                }
+                else if(currentSum>0){
+                    right-=1;
+                }
+            }
+        }
+        return result;
+    }
+
+public static void main(String gg[])
+{
+int[] nums = new int[]{-1,0,1,2,-1,-4};
+List<List<Integer>> res = new ArrayList<List<Integer>>();
+res = threeSum(nums);
+System.out.println(res);
+}
+}
+
+//time complexity O(N2)
+//space complexity O(1)
---
+++ twoSum.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+public static int[] twoSum(int[] nums, int target) {
+
+    int[] result = new int[2];
+    
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    
+    for(int i=0; i<nums.length; i++){
+        
+       if( map.containsKey(target - nums[i])){
+        result[1] = i;
+        result[0] = map.get(target - nums[i]);
+        return result;   
+       }
+        
+        map.put(nums[i] , i);
+    }
+    return result;
+}
+public static void main(String gg[])
+{
+int[] nums = new int[]{2,7,11,15};
+int[] res = new int[2];
+int target = 9;
+res = twoSum(nums,target);
+System.out.println("["+res[0]+","+res[1]+"]");
+System.out.println(Arrays.toString(res));
+}
+
+}
+// time complexity O(n)
+// space complexity O(1)
