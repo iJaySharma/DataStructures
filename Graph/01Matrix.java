063f92b90874ae9bba578784d424f10926718a58
iJaySharma <jay.sharma96100@gmail.com>
2023-08-17 20:49:09 +0530

    dsa

---
+++ Graph/01Matrix.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Solution {
+    public static int[][] updateMatrix(int[][] mat) {
+        int[][] ans = new int[mat.length][mat[0].length];
+        Queue<int[]> q = new LinkedList<>();        
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[i].length;j++){
+                ans[i][j] = -1;
+                if(mat[i][j]==0){
+                    ans[i][j] = 0;
+                    q.add(new int[]{i,j});
+                }
+            }
+        }
+        int dist = 1;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                int[] pos = q.poll();
+                int row = pos[0];
+                int col = pos[1];
+                if(row>0 && ans[row-1][col] == -1){
+                    ans[row-1][col] = dist;
+                    q.add(new int[]{row-1,col});
+                }
+                if(row<mat.length-1 && ans[row+1][col] == -1){
+                    ans[row+1][col] = dist;
+                    q.add(new int[]{row+1,col});
+                }
+                if(col>0 && ans[row][col-1] == -1){
+                    ans[row][col-1] = dist;
+                    q.add(new int[]{row,col-1});
+                }
+                if(col<mat[0].length-1 && ans[row][col+1] == -1){
+                    ans[row][col+1] = dist;
+                    q.add(new int[]{row,col+1});
+                }
+            }
+            dist++;
+        }
+        return ans;
+    }
+public static void main(String[] gg)
+{
+int[][] arr =new int[][]{{0,0,0},{0,1,0},{1,1,1}};
+int[][] ans = Solution.updateMatrix(arr);
+int m = ans.length;
+int n = ans[0].length;
+for(int i=0; i<m;  i++)
+{
+for(int j=0; j<n; j++)
+{
+System.out.println(ans[i][j]);
+}
+}
+}
+}
