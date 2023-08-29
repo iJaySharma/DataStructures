f3525e3ae6b65fb4dc9cdf47909baacd1e9db6e2
iJaySharma <jay.sharma96100@gmail.com>
2023-08-29 23:33:42 +0530

    DSA

---
+++ BackTracking/permutation.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result,nums,0);
+        return result;
+    }
+    
+    public static void backtrack(List<List<Integer>> result , int[] nums, int s){
+        if(s == nums.length){
+            result.add(toList(nums));
+        }
+        for(int i=s;i<nums.length;i++){
+            swap(i,s,nums);
+            backtrack(result,nums,s+1);
+            swap(i,s,nums);
+        }
+    }
+    
+    public static List<Integer> toList(int[] nums){
+        List<Integer> res = new ArrayList<>();
+        for(int i:nums)
+            res.add(i);
+        return res;
+    } 
+    
+    public static void swap(int i, int j, int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+public static void main(String[] gg)
+{
+int[] num = new int[]{1,2,3};
+System.out.println(Solution.permute(num));
+}
+}
